be74449fa8fa7bf2d32515422beb7748e06af193
Jollibeegit <41350147+Jollibeegit@users.noreply.github.com>
2025-04-11 21:26:41 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/wake', async (req, res) => {
   try {
-    const response = await axios.get('http://jollibee.iptime.org:8888/wol?mac=F4-B5-20-1B-EE-5D');
+    const response = await axios.get('http://218.233.206.187:8888/wol?mac=F4-B5-20-1B-EE-5D');
     res.send('✅ WOL 요청 전송 완료!');
   } catch (err) {
     res.status(500).send('❌ 전송 실패: ' + err.message);
@@ -13,5 +13,5 @@ app.get('/wake', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Render WOL 중계 서버 실행 중 on port ${PORT}`);
+  console.log(`🚀 Render WOL 중계 서버 실행 중 on port ${PORT}`);
 });
